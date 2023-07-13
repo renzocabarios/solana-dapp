f5d6e6bbb2e9e26b93cba31ed82629e325d823d2
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-07-13 20:37:01 +0800

    Add phatom wallet connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { PhantomProvider } from "./interfaces";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [provider, setProvider] = useState<PhantomProvider | undefined>(
+    undefined
+  );
 
+  const disconnectWallet = async () => {
+    // @ts-ignore
+    const { solana } = window;
+
+    if (walletKey && solana) {
+      await (solana as PhantomProvider).disconnect();
+      setWalletKey(undefined);
+    }
+  };
+
+  const [walletKey, setWalletKey] = useState<string | undefined>(undefined);
+
+  const connectWallet = async () => {
+    // @ts-ignore
+    const { solana } = window;
+
+    if (solana) {
+      try {
+        const response = await solana.connect();
+        console.log("wallet account ", response.publicKey.toString());
+        setWalletKey(response.publicKey.toString());
+      } catch (err) {}
+    }
+  };
+
+  useEffect(() => {
+    const provider = getProvider();
+
+    if (provider) setProvider(provider);
+    else setProvider(undefined);
+  }, []);
+
+  const getProvider = (): PhantomProvider | undefined => {
+    if ("solana" in window) {
+      // @ts-ignore
+      const provider = window.solana as any;
+      if (provider.isPhantom) return provider as PhantomProvider;
+    }
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <header className="App-header">
+          <h2>Tutorial: Connect to Phantom Wallet</h2>
+          {provider && (
+            <button
+              style={{
+                fontSize: "16px",
+                padding: "15px",
+                fontWeight: "bold",
+                borderRadius: "5px",
+              }}
+              onClick={connectWallet}
+            >
+              Connect to Phantom Wallet
+            </button>
+          )}
+          {provider && walletKey && (
+            <div>
+              <p>Connected account {walletKey}</p>
+
+              <button
+                style={{
+                  fontSize: "16px",
+                  padding: "15px",
+                  fontWeight: "bold",
+                  borderRadius: "5px",
+                  margin: "15px auto",
+                }}
+                onClick={disconnectWallet}
+              >
+                Disconnect
+              </button>
+            </div>
+          )}
+
+          {!provider && (
+            <p>
+              No provider found. Install
+              <a href="https://phantom.app/">Phantom Browser extension</a>
+            </p>
+          )}
+        </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
